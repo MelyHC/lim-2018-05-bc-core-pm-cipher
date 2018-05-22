3ceb180b8ec913d40c4b854a214952d9e30549fc
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-05-22 17:49:13 -0500

    estructura basica

---
+++ src/index.js
@@ -0,0 +1,11 @@
+var textoT = document.getElementById('texto').value
+var bCode = document.getElementById('botonCifrado');
+var bDecode = document.getElementById('botonDescifrado');
+
+bCode.addEventListener('click', function code() {
+    textoT;
+});
+
+bDecode.addEventListener('click', function decode() {
+    document.getElementById('texto').value ='cambiar';
+});
