8f5dca874907d595b65cbf5fe0118f620d3c23a3
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-05-24 18:08:33 -0500

    prueba

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-var textoT = document.getElementById('texto').value
-var bCode = document.getElementById('botonCifrado');
-var bDecode = document.getElementById('botonDescifrado');
+let textoT = document.getElementById('texto');
+const bCode = document.getElementById('botonCifrado');
+const bDecode = document.getElementById('botonDescifrado');
 
 bCode.addEventListener('click', function code() {
-    textoT;
+    let caracter = [];
+    let textoL = textoT.value
+    for (let i = 0; i < textoL.length; i++ ) {
+        caracter[i] = textoL.charCodeAt(i) + 4;
+    }
+    textoL = caracter.toString();
 });
 
 bDecode.addEventListener('click', function decode() {
-    document.getElementById('texto').value ='cambiar';
+    for (let i = 0; i <= textoT.lengt; i++ ) {
+        
+    }
+    textoT.value = 'Cambiar texto para decoficicarlo';
 });
+console.log(textoT)
