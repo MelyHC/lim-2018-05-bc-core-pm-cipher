cc93988325f26d8409966d8da81268ca1611ab16
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-05-29 18:39:22 -0500

    pasando al cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,50 @@
+let textoE = document.getElementById('textoEntrada');
+let textoS = document.getElementById('textoSalida');
+
 window.cipher = {
-  // ... 
-};
+    encode :  () =>  {     
+        let caracter = '';
+        let textoL = textoE.value;
+        for (let i = 0; i < textoL.length; i++ ) {
+            //Obteniendo el código Ascii de cada letra de mi texto
+            let coAscii = textoL.charCodeAt(i); 
+            //Filtro el coAscii para aplicar la fórmula correspondiente y devolver 
+            if(coAscii >= 65 && coAscii <= 90) {
+                let letraMayu = String.fromCharCode((coAscii - 65 + 4) % 26 + 65);
+                caracter += letraMayu;
+            } else if(coAscii >= 97 && coAscii <= 122) {
+                let letraMinu = String.fromCharCode((coAscii - 97 + 4) % 26 + 97);
+                caracter += letraMinu
+            } else if(coAscii >= 48 && coAscii <= 57) {
+                let numero = String.fromCharCode((coAscii - 48 + 4) % 10 + 48);
+                caracter += numero;
+            } else {
+                let oCaracter = String.fromCharCode(coAscii)
+                caracter += oCaracter;
+            }
+        }
+        textoS.value = caracter;
+    },
+
+    decode : () => {
+        let caracter = '';
+        let textoL = textoE.value;
+        for (let i = 0; i < textoL.length; i++ ) {
+            let coAscii = textoL.charCodeAt(i);
+            if(coAscii >= 65 && coAscii <= 90) {
+                let letraMayu = String.fromCharCode((coAscii - 90 - 4) % 26 + 90);
+                caracter += letraMayu;
+            } else if(coAscii >= 97 && coAscii <= 122) {
+                let letraMinu = String.fromCharCode((coAscii - 122 - 4) % 26 + 122);
+                caracter += letraMinu;
+            } else if(coAscii >= 48 && coAscii <= 57) {
+                let numero = String.fromCharCode((coAscii - 57 - 4) % 10 + 57);
+                caracter += numero;
+            } else {
+                let oCaracter = String.fromCharCode(coAscii)
+                caracter += oCaracter;
+            }
+        }
+        textoS.value = caracter;
+    },
+}
--- src/index.js
+++ src/index.js
@@ -1,54 +1,12 @@
 let textoE = document.getElementById('textoEntrada');
+let textoS = document.getElementById('textoSalida');
+
 const bCifrar = document.getElementById('botonCifrado');
 const bDecifrar = document.getElementById('botonDescifrado');
-let textoS = document.getElementById('textoSalida');
+
 
 let letraÑ = String.fromCharCode(165);//UFT-8 usa un código diferente al Ascii para algunas letras
 
-bCifrar.addEventListener('click', function code() {
-    let caracter = '';
-    let textoL = textoE.value;
-    for (let i = 0; i < textoL.length; i++ ) {
-        //Obteniendo el código Ascii de cada letra de mi texto
-        let coAscii = textoL.charCodeAt(i); 
-        if(coAscii >= 65 && coAscii <= 90) {
-            let letraMayu = String.fromCharCode((coAscii - 65 + 4) % 26 + 65);
-            caracter += letraMayu;
-        } else if(coAscii >= 97 && coAscii <= 122) {
-            let letraMinu = String.fromCharCode((coAscii - 97 + 4) % 26 + 97);
-            caracter += letraMinu
-        } else if(coAscii >= 48 && coAscii <= 57) {
-            let numero = String.fromCharCode((coAscii - 48 + 4) % 10 + 48);
-            caracter += numero;
-         /*} else if(coAscii >= 160 && coAscii <= 165) {
-            let intento = String.fromCharCode((coAscii- 160 + 4) % 6 + 160);
-            caracter += intento*/
-        } else {
-            let oCaracter = String.fromCharCode(coAscii)
-            caracter += oCaracter;
-        }
-    }
-    textoS.value = caracter;
-});
+bCifrar.addEventListener('click', cipher.encode(offset, textoE.value))
 
-bDecifrar.addEventListener('click', function decode() {
-    let caracter = '';
-    let textoL = textoE.value;
-    for (let i = 0; i < textoL.length; i++ ) {
-        let coAscii = textoL.charCodeAt(i);
-        if(coAscii >= 65 && coAscii <= 90) {
-            let letraMayu = String.fromCharCode((coAscii - 90 - 4) % 26 + 90);
-            caracter += letraMayu;
-        } else if(coAscii >= 97 && coAscii <= 122) {
-            let letraMinu = String.fromCharCode((coAscii - 122 - 4) % 26 + 122);
-            caracter += letraMinu;
-        } else if(coAscii >= 48 && coAscii <= 57) {
-            let numero = String.fromCharCode((coAscii - 57 - 4) % 10 + 57);
-            caracter += numero;
-        } else {
-            let oCaracter = String.fromCharCode(coAscii)
-            caracter += oCaracter;
-        }
-    }
-    textoS.value = caracter;
-});
+bDecifrar.addEventListener('click', cipher.decode(offset, textoE.value))
