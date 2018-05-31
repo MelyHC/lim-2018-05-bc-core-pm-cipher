5fc02e86df436abfa063b3bdd849b77e2781b214
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-05-30 22:07:34 -0500

    Agregando descripción html

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ let textoS = document.getElementById('textoSalida');
 let numD = document.getElementById('númeroDesplazamiento');
 
 let bCifrar = document.getElementById('botonCifrado');
-let bDecifrar = document.getElementById('botonDescifrado');
+let bDescifrar = document.getElementById('botonDescifrado');
 
 bCifrar.addEventListener('click', function () {
     textoS.value =  cipher.encode(parseInt(numD.value), textoE.value)
 });
-bDecifrar.addEventListener('click', function() {
+bDescifrar.addEventListener('click', function() {
     textoS.value = cipher.decode(parseInt(numD.value), textoE.value)
 });
