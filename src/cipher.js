aa067a8352812501c7a9feeb9196bc8f3e4e50e7
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-08-12 01:54:36 -0500

    Nueva vista y correciones close #1

--- src/cipher.js
+++ src/cipher.js
@@ -1,58 +1,50 @@
 window.cipher = {
-    encode :  (offset, string) =>  {     
-        //Declaro una variable donde se almacenara el texto cifrado
-        let character = '';
-        //Bucle para recorrer el texto ingresado
-        for (let i = 0; i < string.length; i++ ) {
-            //Obteniendo el código Ascii de cada letra del texto ingresado
-            let coAscii = string.charCodeAt(i); 
-            //Evalua que el dato de entrada este entre los parámetros asignados
-            if(coAscii >= 65 && coAscii <= 90) {
-                let capitalLetter = String.fromCharCode((coAscii - 65 + offset) % 26 + 65);
-                character += capitalLetter;
-            } else if(coAscii >= 97 && coAscii <= 122) {
-                let lowerCaseLetter = String.fromCharCode((coAscii - 97 + offset) % 26 + 97);
-                character += lowerCaseLetter
-            } else if(coAscii >= 48 && coAscii <= 57) {
-                let number = String.fromCharCode((coAscii - 48 + offset) % 10 + 48);
-                character += number;
-            } else {
-                let otherCharacter = String.fromCharCode(coAscii)
-                character += otherCharacter;
-            }
-        }
-        return character;
-    },
-
-    decode : (offset, string) => {
-        //Declaro una variable donde se almacenara el texto descifrado
-        let character = '';
-        //Bucle para recorrer el texto ingresado
-        for (let i = 0; i < string.length; i++ ) {
-            //Obteniendo el código Ascii de cada letra del texto ingresado
-            let coAscii = string.charCodeAt(i);
-            //Evalua que el dato de entrada este entre los parámetros asignados
-            if(coAscii >= 65 && coAscii <= 90) {
-                let capitalLetter = String.fromCharCode((coAscii - 90 - offset) % 26 + 90);
-                character += capitalLetter;
-            } else if(coAscii >= 97 && coAscii <= 122) {
-                let lowerCaseLetter = String.fromCharCode((coAscii - 122 - offset) % 26 + 122);
-                character += lowerCaseLetter;
-            } else if(coAscii >= 48 && coAscii <= 57) {
-                let number = String.fromCharCode((coAscii - 57 - offset) % 10 + 57);
-                character += number;
-            } else {
-                let otherCharacter = String.fromCharCode(coAscii)
-                character += otherCharacter;
-            }
-        }
-        return character;
-    },
-
-    createCipherWithOffset: (offset) => {
-        return {
-            encode:(string) => {return cipher.encode(offset, string)},
-            decode:(string) => {return cipher.decode(offset, string)}
-        }
+  encode: (offset, string) => {
+    //Declaro una variable donde se almacenara el texto cifrado
+    let characters = '';
+    //Bucle para recorrer el texto ingresado
+    for (let i = 0; i < string.length; i++) {
+      //Obteniendo el código Ascii de cada letra del texto ingresado
+      let coAscii = string.charCodeAt(i);
+      //Evalua que el dato de entrada este entre los parámetros asignados
+      if (coAscii >= 65 && coAscii <= 90) {
+        characters += String.fromCharCode((coAscii - 65 + offset) % 26 + 65);
+      } else if (coAscii >= 97 && coAscii <= 122) {
+        characters += String.fromCharCode((coAscii - 97 + offset) % 26 + 97);
+      } else if (coAscii >= 48 && coAscii <= 57) {
+        characters += String.fromCharCode((coAscii - 48 + offset) % 10 + 48);
+      } else {
+        characters += String.fromCharCode(coAscii);
+      }
     }
+    return characters;
+  },
+
+  decode: (offset, string) => {
+    //Declaro una variable donde se almacenara el texto descifrado
+    let characters = '';
+    //Bucle para recorrer el texto ingresado
+    for (let i = 0; i < string.length; i++) {
+      //Obteniendo el código Ascii de cada letra del texto ingresado
+      let coAscii = string.charCodeAt(i);
+      //Evalua que el dato de entrada este entre los parámetros asignados
+      if (coAscii >= 65 && coAscii <= 90) {
+        characters += String.fromCharCode((coAscii - 90 - offset) % 26 + 90);
+      } else if (coAscii >= 97 && coAscii <= 122) {
+        characters += String.fromCharCode((coAscii - 122 - offset) % 26 + 122);
+      } else if (coAscii >= 48 && coAscii <= 57) {
+        characters += String.fromCharCode((coAscii - 57 - offset) % 10 + 57);
+      } else {
+        characters += String.fromCharCode(coAscii);
+      }
+    }
+    return characters;
+  },
+
+  createCipherWithOffset: (offset) => {
+    return {
+      encode: (string) => cipher.encode(offset, string),
+      decode: (string) => cipher.decode(offset, string)
+    }
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 //Declaro variables llamando a los id de los input (textarea e input)
-let inputT = document.getElementById('inputText');
-let exitT = document.getElementById('exitText'); 
-let displacementN = document.getElementById('displacementNumber');
+const inputTxt = document.getElementById('input-text');
+const exitTxt = document.getElementById('exit-text');
+const numDisplaceTxt = document.getElementById('displacement-number');
 
 //Declaro variables llamando a los id de los botones
-let encodeB = document.getElementById('encodeButton');
-let decodeB = document.getElementById('decodeButton');
+const encodeBtn = document.getElementById('encode-button');
+const decodeBtn = document.getElementById('decode-button');
+
+//Valido si los compos requeridos son completados
+const validationTextNum = (text, num) => {
+  if (text.length === 0 || num.length === 0) {
+    alert('Debes llenar todos los compos requeridos');
+    return false
+  } else if (!/[1-9]/.exec(num)) {
+    alert('Debes colocar un número en la casilla n de desplazamiento \n Y debe ser diferente de 0')
+    return false
+  } else {
+    return true
+  }
+}
 
 //Creo eventos de los botones para que ejecuten las funciones de cifrado y descifrado
-encodeB.addEventListener('click', () => {
-    exitT.value =  cipher.encode(parseInt(displacementN.value), inputT.value)
+encodeBtn.addEventListener('click', () => {
+  if (validationTextNum(inputTxt.value, numDisplaceTxt.value)) {
+    exitTxt.value = cipher.encode(parseInt(numDisplaceTxt.value), inputTxt.value)
+  }
 });
-decodeB.addEventListener('click', () => {
-    exitT.value = cipher.decode(parseInt(displacementN.value), inputT.value)
+decodeBtn.addEventListener('click', () => {
+  if (validationTextNum(inputTxt.value, numDisplaceTxt.value)) {
+    exitTxt.value = cipher.decode(parseInt(numDisplaceTxt.value), inputTxt.value)
+  }
 });
